90d1ffe266fb43106ebe0f2a01f6ed724449e327
Anja <anjaruetenbudde@icloud.com>
2025-10-08 11:57:00 +0200

    App now returns packing list additionally to classification. Added outline of blog post.

--- app.py
+++ app.py
@@ -13,15 +13,14 @@ keys_list = list(candidate_labels.keys())
 with open("test_data.json", "r") as file:
     packing_data = json.load(file)
 
-# function and gradio app
-model_name = "facebook/bart-large-mnli"
-classifier = pipeline("zero-shot-classification", model=model_name)
-cut_off = 0.5  # used to choose which activities are relevant
+# Load packing item data
+with open("packing_templates_self_supported_offgrid_expanded.json", "r") as file:
+    packing_items = json.load(file)
 
-def classify(#model_name, 
-             trip_descr, cut_off):
-    
-    # Create an empty DataFrame with specified columns
+# function and gradio app
+def classify(model_name, trip_descr, cut_off = 0.5):
+    classifier = pipeline("zero-shot-classification", model=model_name)
+    ## Create and fill dataframe with class predictions
     df = pd.DataFrame(columns=['superclass', 'pred_class'])
     for i, key in enumerate(keys_list):
         if key == 'activities':
@@ -32,16 +31,31 @@ def classify(#model_name,
             result = classifier(trip_descr, candidate_labels[key])
             classes = result["labels"][0]
         df.loc[i] = [key, classes]
-    return df
+
+    ## Look up and return list of items to pack based on class predictions
+    # make list from dataframe column
+    all_classes = [elem for x in df["pred_class"] for elem in (x if isinstance(x, list) else [x])]
+    # look up packing items for each class/key
+    list_of_list_of_items = [packing_items.get(k, []) for k in all_classes]
+    # combine lists and remove doubble entries
+    flat_unique = []
+    for sublist in list_of_list_of_items:
+        for item in sublist:
+            if item not in flat_unique:
+                flat_unique.append(item)
+    # sort alphabetically to notice duplicates
+    sorted_list = sorted(flat_unique)  
+    return df, "\n".join(sorted_list)
 
 demo = gr.Interface(
     fn=classify,
     inputs=[
-        #gr.Textbox(label="Model name", value = "facebook/bart-large-mnli"),
+        gr.Textbox(label="Model name", value = "facebook/bart-large-mnli"),
         gr.Textbox(label="Trip description"),
         gr.Number(label="Activity cut-off", value = 0.5),
     ],
-    outputs="dataframe",
+    # outputs="dataframe",
+    outputs=[gr.Dataframe(label="DataFrame"), gr.Textbox(label="List of words")],
     title="Trip classification",
     description="Enter a text describing your trip",
 )
---
+++ blog_post/article.md
@@ -0,0 +1,45 @@
+# Working with Large Language Models
+## Introduction
+* What are LLMs and why are they popular
+* How to use the open source platform to use LLMs for own application; example of creating a packing list.
+
+## Why bother to adept models to your application
+* Problems of LLMS: Hallucination and wrong outputs
+* Controll outputs by using zero-shot-calssification
+  * briefly mention other types of classification
+* How we do it with our packing list model
+  * Why not use packing items as classes
+  * Use superclasses to categories trip and have packing items correspond to superclasses
+  * Asses performance with small test data set
+  * mention gradio app to make it user friendly and spaces to share model
+
+## Implementation of packing list model
+### Prerequisites before you start to code
+* Hugging face account also use access token, mention api
+* Use anaconda
+* Install python and libraries 
+* create jupyter notebook
+* pip install stuff
+
+### Predefine outputs/classes
+
+### Model implementation
+
+### Using gradio app
+
+### Share your model
+* created space
+* created github access token on huffing face and then github actions workflow
+* you need to install the required dependencies in the environment where your Hugging Face Space is running.
+* create requirements.txt file with necessary libraries
+
+
+## Performance assessment
+* Test data creation
+
+## Side notes
+* Hugging face API (or mention during implementation)
+
+## Closing
+* Summary
+* Limitations
